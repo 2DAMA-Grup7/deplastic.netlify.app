4e4945b67e3d4d0bac7fe9dae4c40f6e332e7117
fedoray <fedoray>
2022-11-15 12:20:22 +0100

    add rows to get

--- src/components/getUser.vue
+++ src/components/getUser.vue
@@ -1,8 +1,37 @@
 <template>
-    <div v-for="user in listUsers">
-      {{user.username}},
-      {{user.roles}}
-    </div>
+    <v-table density="compact">
+    
+    <thead>
+      <tr>
+        <th class="text-left">
+          User ID
+        </th>
+        <th class="text-left">
+          Roles
+        </th>
+        <th class="text-left">
+          Email
+        </th>
+        <th class="text-left">
+          Username
+        </th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="user in listUsers"
+        :key="user.username"
+      >
+      <td class="text-left" >{{ user.USER_ID }}</td>
+        <td class="text-left">{{ user.username }}</td>
+        <td class="text-left" >{{ user.roles }}</td>
+        <td class="text-left" >{{ user.email }}</td>
+        
+      </tr>
+    </tbody>
+    
+  </v-table>
   </template>
 
 

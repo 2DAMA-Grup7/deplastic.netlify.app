f574f46e1a2e08161bb21fab78bf9ecc7d62d543
fedoray <fedoray>
2022-11-29 22:36:17 +0100

    test27

--- src/components/FileReader.vue
+++ src/components/FileReader.vue
@@ -9,11 +9,9 @@
       <tr v-for="item in logs" :key="item">
           </tr>
         </tbody>
-        
       <v-card>
     {{this.logs}}
       </v-card>
-
       <v-card>
     {{logs}}
       </v-card>
@@ -25,10 +23,12 @@
 <script>
 import logtxt from "raw-loader!../../netlify/functions/routes/Log.txt"
 
+
 export default{
 
   data(){
-    return{logs:logtxt.split(/[\n]+/)}
+    //return{logs:logtxt.split(/[\n]+/)}
+    return{logs:["a","b"]}
   }
 }
 

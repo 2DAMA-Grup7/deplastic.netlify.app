c11521c4af701c27330cf5be1debf07d129bfc67
fedoray <fedoray>
2022-11-29 20:40:00 +0100

    test18

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -2,14 +2,8 @@
   <v-card>
     <v-toolbar class="bg-deep-purple" flat>
       <v-toolbar-title>Log</v-toolbar-title>
-      <v-button @click="getLog"></v-button>
-      <v-table>
-        <thead>
-          <tr >
-            <td class="text-left"  ></td>
-          </tr>
-        </thead>
-      </v-table>
+      <v-button @click="getLog">LOG</v-button>
+     
     </v-toolbar>
   </v-card>
 </template>

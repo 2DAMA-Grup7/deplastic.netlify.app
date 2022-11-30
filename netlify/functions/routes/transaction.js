f57ab2b903dca86ab591d0aeabc5f22e7a52b511
a19marsbasgra <a19marsbasgra@inspedralbes.cat>
2022-11-30 18:36:16 +0100

    Token????

--- netlify/functions/routes/transaction.js
+++ netlify/functions/routes/transaction.js
@@ -38,7 +38,7 @@ function remove(req, res) {
 function post(req, res) {
   var database = require("../lib/db");
   database.query(
-    `INSERT INTO Transaccio (nom, url, id, description, price) VALUES ('${req.body.nom}','${req.body.url}',NULL,'${req.body.description}','${req.body.price}`,
+    `INSERT INTO Transaccio (nom, url, id, description, price) VALUES ('${req.body.nom}','${req.body.url}',NULL,'${req.body.description}','${req.body.price})`,
     function (err, result) {
       if (err) {
         res.send(JSON.stringify({ success: false }));

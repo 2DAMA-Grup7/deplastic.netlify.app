1485492deeea6135a1b34cbe099edf07b1746e4f
alexiadltg <a22aledelgar@inspedralbes.cat>
2022-12-02 10:31:23 +0100

    fix marker.vue

--- src/components/Marker.vue
+++ src/components/Marker.vue
@@ -111,10 +111,6 @@ export default {
         })
         .then((data) => {
           console.log(data);
-          let logsItems = localStorage.getItem("logs") || "{}";
-          logsItems = JSON.parse(logsItems);
-          logsItems.push({ logs: "Removed Marker" });
-          localStorage.setItem("logs", JSON.stringify(logsItems));
           window.location.reload();
         });
     },

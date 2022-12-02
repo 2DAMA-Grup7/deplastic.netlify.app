fffa5100697687c9f7daa10ab15e0390a14d896f
Easel7231 <113882951+Easel7231@users.noreply.github.com>
2022-12-02 10:02:22 +0100

    Update User.vue

--- src/components/User.vue
+++ src/components/User.vue
@@ -131,7 +131,7 @@ export default {
             username: this.inputUser.username,
             password: this.inputUser.password,
             email: this.inputUser.email,
-            roles: this.inputUser.roles,
+            roles: inputUser.PointTotal,
           }),
           headers: { "Content-Type": "application/json" },
         })
@@ -158,6 +158,7 @@ export default {
           password: this.inputUser.password,
           email: this.inputUser.email,
           roles: this.inputUser.roles,
+          PointTotal: this.inputUser.roles,
         }),
         headers: { "Content-Type": "application/json" },
       })
